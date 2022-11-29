1513e7e95b7f5f0675a0fbc6d40ae2af32ffd676
TinoETPA <tino.m@hotmail.fr>
2022-11-29 16:21:34 +0100

    Update monScript.js

--- monScript.js
+++ monScript.js
@@ -41,6 +41,5 @@ maDiv.onclick = function() {
     valeurResultat.innerHTML = valeurResultat.innerHTML+10;
     afficheAction.innerHTML = "J'ai ajouté 10 !";
 }
-// Exercice : débuggez ce script :)
 
 
