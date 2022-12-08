b1ecd1f550d695265e3be70128b6058f83191af0
Mio <arnaudhung@gmail.com>
2022-12-08 17:01:54 +0100

    Barre de vie qui baisse, riposte intégrer

--- monScript.js
+++ monScript.js
@@ -1,53 +1,157 @@
 
-pvJoueur1 = document.getElementById("pvJoueur1");
-pvJoueur2 = document.getElementById("pvJoueur2");
-pvJoueur3 = document.getElementById("pvJoueur3");
-pvJoueur4 = document.getElementById("pvJoueur4");
-pvMob1 = document.getElementById("pvMob1");
-pvMob2 = document.getElementById("pvMob2");
-pvMob3 = document.getElementById("pvMob3");
-
-atkHeros1 = document.getElementById("atkHeros1");
-atkHeros2 = document.getElementById("atkHeros2");
-atkHeros3 = document.getElementById("atkHeros3");
-atkHeros4 = document.getElementById("atkHeros4");
-
-affichePvJoueur1 = document.getElementById("affichePvJoueur1");
-affichePvJoueur2 = document.getElementById("affichePvJoueur2");
-affichePvJoueur3 = document.getElementById("affichePvJoueur3");
-affichePvJoueur4 = document.getElementById("affichePvJoueur4");
-affichePvMob1 = document.getElementById("affichePvMob1");
-affichePvMob2 = document.getElementById("affichePvMob2");
-affichePvMob3 = document.getElementById("affichePvMob3");
-
-affichage = document.getElementById("affichage");
-
 // une div affichage pour tous les massages, comme dialogue ff
 // div.onclick machin sur quoi on doit cliquer pas forcement texte
 // div click a changer apres
 
-let pvMonstre1 = document.getElementById("pvMonstre1")
-let pvHero1 = document.getElementById("pvHero1")
+pvMonstre1 = document.getElementById("pvMonstre1")
+monstre1 = document.getElementById("pvMonstre1")
+pvHero1 = document.getElementById("pvHero1")
 
-function fcAttaqueheros1(){
+// Variable pour definir la valeur de l'actions des heros
 
-    pvMonstre1.value -= 10;
-    affichage.innerHTML = "Le monstre perd 10 PV !";
-    affichage.innerHTML += "</br>"+" Le monstre riposte il vous inflige 10 PV !";
-    pvHero1.value -= 10;
+var actionHeros1 = false;
+var actionHeros2 = false;
+var actionHeros3 = false;
+var actionHeros4 = false;
+
+
+// Les fonctions pour les bouton d'actions des personnages jouables
+function fcAttaqueheros1(){ //fonction attaquer du héro
+    if (actionHeros1 == false){
+        pvMonstre1.value -= 10;
+        affichage.innerHTML = "Le monstre perd 10 PV !";
+        actionHeros1 = true;
+        riposteMonstre();
+    }
 }
-
-function defenseheros1(){
-
-    affichage.innerHTML = "Vous ne pouvez pas subir de dégats !";
-
+function defenseheros1(){ // fonction defendre du héro, ne peux pas subir de dégat durant cette phase
+    if (actionHeros1 == false){
+        affichage.innerHTML = "Vous ne pouvez pas subir de dégats !";
+        actionHeros1 = true;
+        riposteMonstre();
+    }
     
 }
 
-function attaqueSpe1(){
+function attaqueSpe1(){// fonction attaque spéciale qui fais plus de dégats
 
-    pvMonstre1.value -= 30;
-    affichage.innerHTML = "Le monstre perd 30 PV !"
+    if (actionHeros1 == false){
+        pvMonstre1.value -= 30;
+        affichage.innerHTML = "Le monstre perd 30 PV !"
+        actionHeros1 = true;
+        riposteMonstre();
+    }
 
 }
 
+
+function fcAttaqueheros2(){
+
+    if (actionHeros2 == false){
+        pvMonstre1.value -= 10;
+        affichage.innerHTML = "Le monstre perd 10 PV !";
+        actionHeros2 = true;
+        riposteMonstre();
+    }
+
+}
+
+function defenseheros2(){
+
+    if (actionHeros2 == false){
+        affichage.innerHTML = "Vous ne pouvez pas subir de dégats !";
+        actionHeros2 = true;
+        riposteMonstre();
+    }  
+
+}
+
+function attaqueSpe2(){
+
+    if (actionHeros2 == false){
+        pvMonstre1.value -= 30;
+        affichage.innerHTML = "Le monstre perd 30 PV !"
+        actionHeros2 = true;
+        riposteMonstre();
+    } 
+
+}
+
+function fcAttaqueheros3(){
+
+    if (actionHeros3 == false){
+        pvMonstre1.value -= 10;
+        affichage.innerHTML = "Le monstre perd 10 PV !";
+        actionHeros3 = true;
+        riposteMonstre();
+    } 
+
+}
+
+function defenseheros3(){
+
+    if (actionHeros3 == false){
+        affichage.innerHTML = "Vous ne pouvez pas subir de dégats !";
+        actionHeros3 = true;
+        riposteMonstre();
+    } 
+    
+}
+
+function attaqueSpe3(){
+    if (actionHeros3 == false){
+        pvMonstre1.value -= 30;
+        affichage.innerHTML = "Le monstre perd 30 PV !"
+        actionHeros3 = true;
+        riposteMonstre();
+    } 
+
+}
+
+function fcAttaqueheros4(){
+
+    if (actionHeros4 == false){
+        pvMonstre1.value -= 10;
+        affichage.innerHTML = "Le monstre perd 10 PV !";
+        actionHeros4 = true;
+        riposteMonstre();
+    } 
+}
+
+function defenseheros4(){
+
+    if (actionHeros4 == false){
+        affichage.innerHTML = "Vous ne pouvez pas subir de dégats !";
+        actionHeros4 = true;
+        riposteMonstre();
+    } 
+    
+}
+
+function attaqueSpe4(){
+
+    if (actionHeros4 == false){
+        pvMonstre1.value -= 30;
+        affichage.innerHTML = "Le monstre perd 30 PV !";
+        actionHeros4 = true;
+        riposteMonstre()
+    } 
+
+}
+
+// riposte des monstres quand chaque héro a attaquer
+function riposteMonstre(){
+
+    if ((actionHeros1 && actionHeros2) && (actionHeros3 && actionHeros4)){
+
+        pvHero1.value -= 10
+        console.log(pvHero1);
+        affichage.innerHTML = "Les monstres ripostes et inflige 5 Pv chacun";
+
+        actionHeros1 = false;
+        actionHeros2 = false;
+        actionHeros3 = false;
+        actionHeros4 = false;
+    }
+
+}
